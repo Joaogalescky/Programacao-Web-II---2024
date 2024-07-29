a6c8e184c80cd7b1490c5b532c64601d1885decc
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-07-28 21:04:51 -0300

    Update Questao1.py

--- Provas/Rec/Questao1.py
+++ Provas/Rec/Questao1.py
@@ -17,4 +17,7 @@ N = int(input("Digite um número inteiro para a altura do triângulo: "))
 for i in range(N): # altura
     for j in range(i + 1): # comprimento
         print("*", end = " ") # juntar com o outro print, após o 1
+        # O valor de i + 1 determina quantos * serão impressos na linha atual. 
+        # Por exemplo, na primeira iteração, i é 0 e i + 1 é 1, então apenas um * será impresso; 
+        # Na segunda iteração, i é 1 e i + 1 é 2, então dois * serão impressos, e assim por diante.
     print()
