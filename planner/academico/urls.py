08c867dad8b6526cc0f21125b6d439e9b861a25b
Joaogalescky <joaocvgalescky@gmail.com>
2024-08-23 20:15:44 -0300

    Aula 23/08/2024 - Rest (2)

--- planner/academico/urls.py
+++ planner/academico/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfessorView
+from .views import ProfessorView, ProfessorReadUpdateDeleteView
 
 urlpatterns = [
     path('professor', ProfessorView.as_view()),
+    path('professor/<int:id>/', ProfessorReadUpdateDeleteView.as_view(), name='professor-detail'),
 ]
--- planner/academico/views.py
+++ planner/academico/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 #importando modelos e serializers
 from .models import Professor
@@ -9,18 +10,58 @@ from .serializers import ProfessorSerializer
 # Create your views here.
 class ProfessorView(APIView):
 
-    #define as ações quando recebe um requisicao do tipo post
+    # Define as ações quando recebe um requisicao do tipo post
+    
+    # POST
     def post(self, request):
 
-        #instancia o serialize com os dados recebidos no 'request'
+        # Instancia o serialize com os dados recebidos no 'request'
         serializer = ProfessorSerializer(data = request.data)
         if serializer.is_valid():
 
-            #se o formato recebido estiver correto, salva os dados no banco de dados
+            # Se o formato recebido estiver correto, salva os dados no banco de dados
             serializer.save()
 
-            #retorna com o codigo 201 e os dados do serializer
+            # Retorna com o codigo 201 e os dados do serializer
             return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-        #se o serializer não for valido, retorna erro 400
+        # Se o serializer não for valido, retorna erro 400
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    # GET
+    def get(self, request):
+        professors = Professor.objects.all()
+        serializer = ProfessorSerializer(professors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ProfessorReadUpdateDeleteView(APIView):
+# View para recuperar, atualizar ou deletar um professor específico.
+
+    # GET
+    def get(self, request, id):
+        professor = get_object_or_404(Professor, pk=id)
+        
+        '''
+            try:
+                professor = Professor.objects.get(pk=pk)
+            except Professor.DoesNotExist:
+                return Response({'detail': 'Professor não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+        '''
+        
+        serializer = ProfessorSerializer(professor)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # PUT
+    def put(self, request, id):
+        professor = get_object_or_404(Professor, pk=id)
+        serializer = ProfessorSerializer(professor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # DELETE
+    def delete(self, request, id):
+        professor = get_object_or_404(Professor, pk=id)
+        professor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
