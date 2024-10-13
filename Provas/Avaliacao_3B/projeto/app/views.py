341fecc23f84bc968ffcb467b28e39b903f05590
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-13 00:00:15 -0300

    refactor: views

--- Provas/Avaliacao_3B/projeto/app/views.py
+++ Provas/Avaliacao_3B/projeto/app/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Autor, Livro, Tag
-from .serializers import AutorSerializer, AutorLivroCreateUpdateSerializer, LivroSerializer, LivroTagCreateUpdateSerializer, TagSerializer
+from .serializers import AutorSerializer, LivroSerializer, TagSerializer
 
 # Create your views here.
 # CRUD - Autor
@@ -15,7 +15,7 @@ class AutorView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.erros, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
     
     # GET
     def get(self, request):
@@ -37,7 +37,7 @@ class AutorReadUpdateDeleteView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # DELETE
     def delete(self, request, id):
@@ -53,7 +53,7 @@ class LivroView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # GET
     def get(self, request):
@@ -87,51 +87,37 @@ class LivroCreateWithAutorView(APIView):
     # POST
     def post(self, request):
         autor_data = request.data.pop('autor')
-        autor_serializer = LivroSerializer(data=autor_data)
+        livro_data = request.data
     
-        if livro_serializer.is_valid():
-            livro = livro_serializer.save()
-            
-            # Vincular o livro ao autor
-            livro_data = request.data
-            livro_data['autor'] = livro.id # Associa o autor
+        # Serializar o autor (caso seja novo)
+        autor_serializer = AutorSerializer(data=autor_data)
+        if autor_serializer.is_valid():
+            autor = autor_serializer.save()
+            livro_data = request.data.copy()
+            livro_data['autor'] = autor.id
+
             livro_serializer = LivroSerializer(data=livro_data)
-            
             if livro_serializer.is_valid():
                 livro_serializer.save()
                 return Response(livro_serializer.data, status=status.HTTP_201_CREATED)
+
             return Response(livro_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(autor_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    # GET
-    def get(self, request):
-        livros = Livro.objects.all()
-        serializer = LivroSerializer(livros, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # CRUD - Tag
 class TagCreateWithLivroView(APIView):
     # POST
     def post(self, request):
-        livro_data = request.data.pop('livro')
-        livro_serializer = LivroSerializer(data=livro_data)
+        tags_data = request.data.pop('tags')
+        livro_data = request.data
     
+        livro_serializer = LivroSerializer(data=livro_data)
         if livro_serializer.is_valid():
             livro = livro_serializer.save()
             
             # Vincular a tag ao livro
-            tag_data = request.data
-            tag_data['livro'] = livro.id # Associa o livro
-            tag_serializer = TagSerializer(data=tag_data)
-            
-            if tag_serializer.is_valid():
-                tag_serializer.save()
-                return Response(tag_serializer.data, status=status.HTTP_201_CREATED)
-            return Response(tag_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            for tag_data in tags_data:
+                tag, created = Tag.objects.get_or_create(nome=tag_data['nome'])
+                livro.tags.add(tag)
+            return Response(LivroSerializer(livro).data, status=status.HTTP_201_CREATED)
         return Response(livro_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    # GET
-    def get(self, request):
-        tags = Tag.objects.all()
-        serializer = TagSerializer(tags, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
