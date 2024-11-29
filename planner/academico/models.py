b9da108ab92ffddea2ba9baad90252e47a715ed9
Joaogalescky <joaocvgalescky@gmail.com>
2024-11-29 20:04:09 -0300

    style: models

--- planner/academico/models.py
+++ planner/academico/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Professor(models.Model):
     nome = models.CharField(max_length = 200)
     email = models.CharField(max_length = 200)
+    
     def __str__(self):
         return self.nome
 
